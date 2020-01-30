fcf0eb434c4af4c0e6b9a7af07c160b3b70313c0
Rani Basna <ranibasna@yahoo.com>
2020-01-30 21:15:13 +0100

    creating the UFT function

--- R_Functions/Functions.R
+++ R_Functions/Functions.R
@@ -57,6 +57,48 @@ bined_converted_func <-  function(converted_data, original_data){
   #cbind(food_cate_to_num, rand_food_data2[rand_food_spilts$numeric] %>% scale()) 
 }
 
+# checks: we need to check that the data do not have a coloumn with a single value.
+# we need to drop the numerical values and check that on the data and maybe filtering that
+# check how to add a seed
+# library required dplyer
 
+UFT_func <- function(Data, Seed){
+  set.seed(Seed)
+  # Spliting the data according to their classes 
+  data_splits <- Data_Classes_Split(Data)
+  drops_numerical <-data_splits$numeric
+  # droping numerical classes
+  data_cate <- Data[ , !(names(Data) %in% drops_numerical)]
+  m <- length(data_cate)
+  d <- dim(data_cate)[1]
+  #
+  for (j in 1:m) {
+    n <- length(unique(data_cate[,j])) 
+    S <- as.vector(as.matrix(dplyr::count(data_cate, data_cate[,j], sort = TRUE)[,1])) # the unique values of variable (we did this some that the order match with count and probabilities)
+    C <- as.vector(as.matrix(dplyr::count(data_cate, data_cate[,j], sort = TRUE)[,2])) # the count (number of occurance) of the values in the variable
+    P <- C/(d*1) # Probabilities
+    for (i in 1:n){
+      mu <- 0
+      L1 <- 0
+      L2 <- 1- sum(P^3)
+      L3 <- 0
+      for (h in 1:n) {
+        H <- P[h]*P[i]*(h-i)^2
+        L3 <- H + L3
+      }
+      for (k in 1:i){
+        mu1 <- sum((n-k) * P[k])
+        L1 <- L1 + mu1
+      }
+      mu <- ((n -i) - L1) * sqrt(L2/L3)
+      for (l in 1:d){
+        if (data_cate[l,j] == S[i]){
+          data_cate[l,j] <- rnorm(1,mu, P[i])
+        }
+      }
+    }
+  }
+  return(data_cate)
+}
 
 
